725bf17dbd58945ab6daebc70984790268134ea8
guzmandp <guzmandp@gmail.com>
2022-01-18 13:16:53 -0300

    index

--- ocean-backend-introducao-18-01-2022/index.js
+++ ocean-backend-introducao-18-01-2022/index.js
@@ -4,14 +4,17 @@ const { MongoClient, ObjectId } = require("mongodb");
 const app = express()
 const port = 3000
 
-const mongoURL = "mongodb+srv://admin:hd2rV5duoPrrli3t@cluster0.jup2c.mongodb.net/";
-const dbName = "ocena_bancodados_18_12_2022";
-
+const mongoURL = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+const dbName = "ocena_bancodados_18_01_2022_darwin";
 
 async function main() {
-
     //cluster0.4uek1.mongodb.net
 
+    const client = await MongoClient.connect(mongoURL);
+    
+    const db = client.db(dbName);
+
+    const collection = db.collection("herois")
 
     app.use(express.json());
 
@@ -19,43 +22,50 @@ async function main() {
         res.send('Hello World!')
     });
 
-    const listaNomeHerois = ["Mulher Maravilha", "Capitã Marvel", "Homen de Ferro"];
-
-    app.get("/herois", function (req, res) {
+    app.get("/herois", async function (req, res) {
     
-        res.send(listaNomeHerois)
+        const documentos = await collection.find().toArray();
+        res.send(documentos)
     });
 
-    app.get("/herois/:id", function (req, res) {
-        const id = req.params.id - 1;
+    app.get("/herois/:id", async function (req, res) {
+        const id = req.params.id;
     
-        const item = listaNomeHerois[id]
+        const item = await collection.findOned({_id: new ObjectId(id)});
     
         res.send(item)
     });
 
-    app.post("/herois", function (req, res) {
-        const item = req.body.nome;
+    app.post("/herois", async function (req, res) {
+        const item = req.body;
 
-        listaNomeHerois.push(item);
+       await collection.insertOne(item)
 
         res.send("item adicionado com sucesso.")
     });
 
-    app.put("/herois/:id", function (req, res) {
-        const id = req.params.id - 1;
+    app.put("/herois/:id", async function (req, res) {
+        const id = req.params.id;
     
         const item = req.body;
-
-        listaNomeHerois[id] = item.nome;
+       
+       await collection.updateOne(
+           { _id: new ObjectId(id) },
+           
+           {
+                $set: item,
+           }
+        );
     
         res.send("Item atualizado com sucesso!")
     });
 
-    app.delete("/herois/:id", function (req, res) {
-        const id = req.params.id - 1;
+    app.delete("/herois/:id", async function (req, res) {
+        const id = req.params.id;
     
-        delete listaNomeHerois[id]
+        const isFound = await collection.countDocuments({ _id: new ObjectId(id) });
+
+        await collection.deleteOne({ _id: new ObjectId(id) })
     
         res.send("Item deletado com sucesso!")
     });
