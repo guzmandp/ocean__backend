303a377d2fdf7874a5caa1e4988957d9f9d0f362
guzmandp <guzmandp@gmail.com>
2022-01-18 11:11:57 -0300

    configuration mongodb

--- ocean-backend-introducao-18-01-2022/index.js
+++ ocean-backend-introducao-18-01-2022/index.js
@@ -1,55 +1,69 @@
 const express = require('express')
+const { MongoClient, ObjectId } = require("mongodb");
+
 const app = express()
 const port = 3000
 
-app.use(express.json());
+const mongoURL = "mongodb+srv://admin:hd2rV5duoPrrli3t@cluster0.jup2c.mongodb.net/";
+const dbName = "ocena_bancodados_18_12_2022";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
 
-const listaNomeHerois = ["Mulher Maravilha", "Capitã Marvel", "Homen de Ferro"];
+async function main() {
 
-app.get("/herois", function (req, res) {
+    //cluster0.4uek1.mongodb.net
+
+
+    app.use(express.json());
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
+
+    const listaNomeHerois = ["Mulher Maravilha", "Capitã Marvel", "Homen de Ferro"];
+
+    app.get("/herois", function (req, res) {
     
-    res.send(listaNomeHerois)
-});
+        res.send(listaNomeHerois)
+    });
 
-app.get("/herois/:id", function (req, res) {
-    const id = req.params.id - 1;
+    app.get("/herois/:id", function (req, res) {
+        const id = req.params.id - 1;
     
-    const item = listaNomeHerois[id]
+        const item = listaNomeHerois[id]
     
-    res.send(item)
-});
+        res.send(item)
+    });
 
-app.post("/herois", function (req, res) {
-    const item = req.body.nome;
+    app.post("/herois", function (req, res) {
+        const item = req.body.nome;
 
-    listaNomeHerois.push(item);
+        listaNomeHerois.push(item);
 
-    res.send("item adicionado com sucesso.")
-});
+        res.send("item adicionado com sucesso.")
+    });
 
-app.put("/herois/:id", function (req, res) {
-    const id = req.params.id - 1;
+    app.put("/herois/:id", function (req, res) {
+        const id = req.params.id - 1;
     
-    const item = req.body;
+        const item = req.body;
 
-    listaNomeHerois[id] = item.nome;
+        listaNomeHerois[id] = item.nome;
     
-    res.send("Item atualizado com sucesso!")
-});
+        res.send("Item atualizado com sucesso!")
+    });
 
-app.delete("/herois/:id", function (req, res) {
-    const id = req.params.id - 1;
+    app.delete("/herois/:id", function (req, res) {
+        const id = req.params.id - 1;
     
-    delete listaNomeHerois[id]
+        delete listaNomeHerois[id]
     
-    res.send("Item deletado com sucesso!")
-});
+        res.send("Item deletado com sucesso!")
+    });
 
 
-app.listen(port, () => {
-  console.log(`Iniciando na porta http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Iniciando na porta http://localhost:${port}`);
+    });
+}
+
+main();
